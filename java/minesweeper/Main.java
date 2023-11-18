9aa7dbcf180cb8f5b12cfd5ed38ea2b79f22638d
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-11-18 12:30:42 +0100

    print field

--- java/minesweeper/Main.java
+++ java/minesweeper/Main.java
@@ -1,7 +1,43 @@
 package minesweeper;
 
 public class Main {
+
+    private static final int FIELD_WIDTH = 9;
+    private static final int FIELD_HEIGHT = 9;
+
+    private static final int NUMBER_OF_MINES = 10;
+
     public static void main(String[] args) {
-        // write your code here
+        printField(getField());
+    }
+
+    private static char[][] getField() {
+        var field = new char[FIELD_HEIGHT][FIELD_WIDTH];
+        var fieldsToSkip = FIELD_HEIGHT * FIELD_WIDTH / NUMBER_OF_MINES;
+        var counter = fieldsToSkip / 2;
+
+        for (int i = 0; i < FIELD_HEIGHT; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                if (counter < fieldsToSkip) {
+                    field[i][j] = '.';
+                    counter++;
+                } else {
+                    field[i][j] = 'X';
+                    counter = 1;
+                }
+            }
+        }
+
+        return field;
+    }
+
+    private static void printField(char[][] field) {
+        for (var row: field) {
+            for (char cell :
+                    row) {
+                System.out.print(cell);
+            }
+            System.out.println();
+        }
     }
 }
